fe318417264b80c25043312edffc474cbcae1dcc
fll <fllning@163.com>
2024-05-02 18:00:10 +0800

    feat: 使用koa对博客项目进行重构

--- app.js
+++ app.js
@@ -1,44 +1,71 @@
-const Koa = require('koa')
-const app = new Koa()
-const views = require('koa-views')
-const json = require('koa-json')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
+const Koa = require("koa");
+const app = new Koa();
+const views = require("koa-views");
+const json = require("koa-json");
+const onerror = require("koa-onerror");
+const bodyparser = require("koa-bodyparser");
+const logger = require("koa-logger");
+const session = require("koa-generic-session");
+const redisStore = require("koa-redis");
+const { REDIS_CONFIG } = require("./config/db");
 
-const index = require('./routes/index')
-const users = require('./routes/users')
+const index = require("./routes/index");
+const users = require("./routes/users");
+const blog = require("./routes/blog");
+const user = require("./routes/user");
 
 // error handler
-onerror(app)
+onerror(app);
 
 // middlewares
-app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
-}))
-app.use(json())
-app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
+app.use(
+  bodyparser({
+    enableTypes: ["json", "form", "text"],
+  })
+);
+app.use(json());
+app.use(logger());
+app.use(require("koa-static")(__dirname + "/public"));
 
-app.use(views(__dirname + '/views', {
-  extension: 'pug'
-}))
+app.use(
+  views(__dirname + "/views", {
+    extension: "pug",
+  })
+);
 
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+// session 配置
+app.keys = ["some_secret_Luck_123"];
+app.use(
+  session({
+    cookie: {
+      path: "/",
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+    // 配置redis
+    store: redisStore({
+      // all: "127.0.0.1:6379", // 写死本地的redis地址
+      all: `${REDIS_CONFIG.host}:${REDIS_CONFIG.port}`,
+    }),
+  })
+);
 
 // routes
-app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
+app.use(index.routes(), index.allowedMethods());
+app.use(users.routes(), users.allowedMethods());
+app.use(blog.routes(), blog.allowedMethods());
+app.use(user.routes(), user.allowedMethods());
 
 // error-handling
-app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+app.on("error", (err, ctx) => {
+  console.error("server error", err, ctx);
 });
 
-module.exports = app
+module.exports = app;
